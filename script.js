e4740169c4253c791bba3b0a2f55b8cb542f3b87
AnIntellectualBeing <111394690+AnIntellectualBeing@users.noreply.github.com>
2023-05-05 10:17:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,14 @@ Array.from(buttons).forEach((button) => {
       string = eval(string);
       document.querySelector('input').value = string;
     }
-    console.log(e.target)
-    string = string + e.target.innerHTML;
-    document.querySelector('input').value = string;
-  }) 
+    else if (e.target.innerHTML == 'C') {
+      string = "";
+      document.querySelector('input').value = string;
+    }
+    else {
+      console.log(e.target)
+      string = string + e.target.innerHTML;
+      document.querySelector('input').value = string;
+    }
+  })
 })
